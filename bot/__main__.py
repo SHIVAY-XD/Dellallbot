0df541887997656e7ea184fa2b8a6c5eda793ec0
тєαм ѕтαя <115808731+TEAMSTARWORLD@users.noreply.github.com>
2022-10-18 22:19:17 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,6 +1,6 @@
 #  MIT License
 #
-#  Copyright (c) 2019-2020 Dan <https://github.com/delivrance>
+#  Copyright (c) 2019-2020 Dan <https://github.com/TEAMSTARWORLD>
 #
 #  Permission is hereby granted, free of charge, to any person
 #  obtaining a copy of this software and associated
